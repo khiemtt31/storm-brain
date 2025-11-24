c88def73c4249776ab3e4f33394b686856d6ea99
ttkhiem <ttkhiem310503@gmail.com>
2025-11-24 20:44:30 +0700

    task: amusing joke

--- codeforces/contests/BGN001/amusing-joke.cpp
+++ codeforces/contests/BGN001/amusing-joke.cpp
@@ -1,3 +1,28 @@
 /*
 * https://codeforces.com/group/yg7WhsFsAp/contest/355495/problem/D
 */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    bool flag = false;
+
+    string a, b, c;
+
+    cin >> a >> b >> c;
+
+    string ab = a + b;
+    sort(ab.begin(), ab.end());
+    sort(c.begin(), c.end());
+
+    if (ab == c) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
